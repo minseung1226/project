19de004046cab98122850821baa5f564d4b2ecda
minseung1226 <minseung1226@naver.com>
2023-05-06 23:11:59 +0900

    Inquiry(문의사항) 엔티티 추가 ,home html 제작

---
+++ src/main/java/project/project/domain/Inquiry.java
@@ -0,0 +1,31 @@
+package project.project.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Inquiry {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "inquiry_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public Inquiry(User user, Room room) {
+        this.user = user;
+        this.room = room;
+    }
+}
