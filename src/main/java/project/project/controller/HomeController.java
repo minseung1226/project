ba501128533e9fff797c7c13dd95205b2b7f2d53
minseung1226 <minseung1226@naver.com>
2023-06-06 23:27:12 +0900

    지도 검색 기능 개발

--- src/main/java/project/project/controller/HomeController.java
+++ src/main/java/project/project/controller/HomeController.java
@@ -2,14 +2,16 @@ package project.project.controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String test(HttpSession session)
+    public String home(String search)
     {
+        if(!StringUtils.hasText(search)) search="서울역";
         return "home";
     }
 
