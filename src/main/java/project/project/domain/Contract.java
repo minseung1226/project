2467de7a6d457cfaab5533779ad0859f475f4e24
minseung1226 <minseung1226@naver.com>
2023-07-31 23:41:11 +0900

    Contract 도메인 개발

--- src/main/java/project/project/domain/Contract.java
+++ src/main/java/project/project/domain/Contract.java
@@ -9,6 +9,7 @@ import org.w3c.dom.stylesheets.LinkStyle;
 import project.project.domain.baseentity.BaseEntity;
 import project.project.domain.embeded.Address;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +28,31 @@ public class Contract extends BaseEntity {
     private User user;
 
     private String roomAddress;
-    private
+    private String designation; // 지목
+    private String landRightsRatio; // 대지권 비율
+    private Double landSize; // 토지면적
+
+    private String structureType ;// 건물 구조
+    private String purpose; // 용도
+    private Double buildingSize; // 건물 면적
+
+    private String rentalArea; // 임대할 부분
+    private Double roomSize; // 임대 면적
+
+    private Integer deposit; // 보증금
+    private Integer earnestMoney; // 계약금
+    private Integer installmentPayment; // 중도금
+    private Integer finalMoney; // 잔금
+    private Integer monthlyRent; // 월세
+
+    private LocalDate finalMoneyDate; // 잔금일
+    private LocalDate moveInDate;  //입주일
+    private LocalDate contractDate; //계약일
+    private Integer monthlyRentDate; //월세 지급일
+    private String monthlyRentType ;//월세 선불인지 후불인지의 여부
+    private Integer  contractPeriod; //계약기간
+
+    private List<String> specialAgreement;
 
 
 }
